0adb33d6e3be5591365623705f33289a73814695
Michael Reynolds <reynoldsm88@gmail.com>
2016-08-25 19:07:08 -0400

    service should use port provided by the environment

--- credit-approval-service/src/main/scala/org/mireynol/credit/service/Main.scala
+++ credit-approval-service/src/main/scala/org/mireynol/credit/service/Main.scala
@@ -5,14 +5,24 @@ import akka.io.IO
 import spray.can.Http
 import akka.pattern.ask
 import akka.util.Timeout
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.duration._
+import scala.util.Properties
 
 object Main extends App {
+
+  val logger = LoggerFactory.getLogger( classOf[ App ] )
+
+  logger.info( "Starting Credit Approval Example REST service" )
+
   implicit val timeout = Timeout( 5.seconds )
   implicit val system = ActorSystem( "credit-approval-services" )
   val service = system.actorOf( Props[ CreditServiceActor ], "credit-approval-service" )
-  val port = 8080
+
+  val port = Properties.envOrElse( "$OPENSHIFT_PORT", "8080" ).toInt
+  logger.info( "HTTP Port is: " + port )
 
   IO( Http ) ? Http.Bind( service, interface = "0.0.0.0", port = port )
+
 }
